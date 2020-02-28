4be08c3c124acd2f3a2834995daac74fb3f20bb2
BuildTools <unconfigured@null.spigotmc.org>
2020-02-28 18:22:28 -0500

    init

---
+++ index.js
@@ -0,0 +1,240 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+const token = 'NjgzMDM2MjQ0Nzk5NzgyOTM2.XllueQ.B5-6b1RCneWZDIeCw4eIXnKMTHY';
+
+const blacklist = 
+
+bot.on('ready', () =>{
+    console.log('This bot is online!');
+})
+
+bot.on('message', msg=>{
+    if(msg.author.bot){
+        return;
+    }
+    if(msg.content.includes("BAM")){
+        msg.channel.sendMessage(`HERE'S MY DICK`);
+    }
+    if(msg.content.includes("BAA")){
+        msg.channel.sendMessage(`AAAGGGHHH MY FUCKING COCK`);
+    }
+    if(msg.content.includes("BAAA")){
+        msg.channel.sendMessage(`AAAGGGHHH MY FUCKING COCK`);
+    }
+    if(msg.content.includes("RACIST")){
+        msg.channel.sendMessage(`I'VE HAD IT, I'VE FUCKING HAD IT`);
+    }
+    if(msg.content.includes("HAD IT")){
+        msg.channel.sendMessage(`I'M NOT RACIST`);
+    }
+    if(msg.content.includes("bam")){
+        msg.channel.sendMessage(`HERE'S MY DICK`);
+    }
+    if(msg.content.includes("baa")){
+        msg.channel.sendMessage(`AAAGGGHHH MY FUCKING COCK`);
+    }
+    if(msg.content.includes("baaa")){
+        msg.channel.sendMessage(`AAAGGGHHH MY FUCKING COCK`);
+    }
+    if(msg.content.includes("racist")){
+        msg.channel.sendMessage(`I'VE HAD IT, I'VE FUCKING HAD IT`);
+    }
+    if(msg.content.includes("had it")){
+        msg.channel.sendMessage(`I'M NOT RACIST`);
+    }
+    if(msg.content.includes("fuck you whore")){
+        msg.channel.sendMessage(`WE LIKE FORTNITE WE LIKE FORTNITE WE LIKE FORTNITE`);
+    }
+    if(msg.content.includes("FUCK YOU WHORE")){
+        msg.channel.sendMessage(`WE LIKE FORTNITE WE LIKE FORTNITE WE LIKE FORTNITE`);
+    }
+    if(msg.content.includes("ANTHONY YOU FUCKING BITCH")){
+        msg.channel.sendMessage(`YOU'RE TURNING ME INTO A GODDAMN INFAMOUS MEME PAGE.`);
+    }
+    if(msg.content.includes("Anthony you fucking bitch")){
+        msg.channel.sendMessage(`YOU'RE TURNING ME INTO A GODDAMN INFAMOUS MEME PAGE.`);
+    }
+    if(msg.content.includes("ANTHONY, YOU FUCKING BITCH")){
+        msg.channel.sendMessage(`YOU'RE TURNING ME INTO A GODDAMN INFAMOUS MEME PAGE.`);
+    }
+    if(msg.content.includes("anthony you fucking bitch")){
+        msg.channel.sendMessage(`YOU'RE TURNING ME INTO A GODDAMN INFAMOUS MEME PAGE.`);
+    }
+    if(msg.content.includes("ANTHONY, YOU FUCKING BITCH.")){
+        msg.channel.sendMessage(`YOU'RE TURNING ME INTO A GODDAMN INFAMOUS MEME PAGE.`);
+    }
+    if(msg.content.includes("anthony, you fucking bitch")){
+        msg.channel.sendMessage(`YOU'RE TURNING ME INTO A GODDAMN INFAMOUS MEME PAGE.`);
+    }
+    if(msg.content.includes("sheep")){
+        msg.channel.sendMessage(`AAAGGGHHH MY FUCKING COCK`);
+    }
+    if(msg.content.includes("SHEEP")){
+        msg.channel.sendMessage(`AAAGGGHHH MY FUCKING COCK`);
+    }
+    if(msg.content.includes("HAVE FUCKING AUTISM")){
+        msg.channel.sendMessage(`SO YOU GUYS ARE DEALING WITH SOMEBODY, WITH A *FUCKING DISABILITY* RIGHT HERE.`);
+    }
+    if(msg.content.includes("have fucking autism")){
+        msg.channel.sendMessage(`SO YOU GUYS ARE DEALING WITH SOMEBODY, WITH A *FUCKING DISABILITY* RIGHT HERE.`);
+    }
+    if(msg.author.bot){
+        return;
+    }
+    if(msg.content.includes("Felix")){
+        msg.channel.sendMessage(`AAAAAAASSSSSSSSHHHHHHOOOLLLLLLEEEE`);
+    }
+    if(msg.author.bot){
+        return;
+    }
+    if(msg.content.includes("felix")){
+        msg.channel.sendMessage(`AAAAAAASSSSSSSSHHHHHHOOOLLLLLLEEEE`);
+    }
+    if(msg.author.bot){
+        return;
+    }
+    if(msg.content.includes("nigger")){
+        msg.channel.sendMessage(`bruh`);
+    }
+    if(msg.content.includes("niggers")){
+        msg.channel.sendMessage(`bruh`);
+    }
+    if(msg.content.includes("NIGGER")){
+        msg.channel.sendMessage(`bruh`);
+    }
+    if(msg.content.includes("NIGGERS")){
+        msg.channel.sendMessage(`bruh`);
+    }
+    if(msg.content.includes("What's Alberto's Instagram?")){
+        msg.channel.sendMessage(`BRUH A FUCKING HEX ERROR: 4d7920496e7374616772616d2069732040626c61636b73747269706531393736`);
+    }
+    if(msg.content.includes("blackstripe1976")){
+        msg.channel.sendMessage(`WHY THE FUCK WOULD YOU LEAK MY INSTAGRAM YOU ASSHOLE`);
+    }
+    if(msg.content.includes("make those socks wet")){
+        msg.channel.sendMessage(`;))`);
+    }
+    if(msg.content.includes("MAKE THOSE SOCKS WET")){
+        msg.channel.sendMessage(`;))`);
+    }
+    if(msg.content.includes("bruh")){
+        msg.channel.sendMessage(`bruh`);
+    }
+    if(msg.content.includes("BRUH")){
+        msg.channel.sendMessage(`BRUH`);
+    }
+    if(msg.content.includes("GARFIELDEATS")){
+        msg.channel.sendMessage(`FUCKING ASSHOLES NEED TO SALT THEIR FUCKING FRIES MORE AAAAGGGGHHHHH`);
+    }
+    if(msg.content.includes("GarfieldEats")){
+        msg.channel.sendMessage(`FUCKING ASSHOLES NEED TO SALT THEIR FUCKING FRIES MORE AAAAGGGGHHHHH`);
+    }
+    if(msg.content.includes("garfieldeats")){
+        msg.channel.sendMessage(`FUCKING ASSHOLES NEED TO SALT THEIR FUCKING FRIES MORE AAAAGGGGHHHHH`);
+    }
+    if(msg.content.includes("GARFIELD EATS")){
+        msg.channel.sendMessage(`FUCKING ASSHOLES NEED TO SALT THEIR FUCKING FRIES MORE AAAAGGGGHHHHH`);
+    }
+    if(msg.content.includes("Garfield Eats")){
+        msg.channel.sendMessage(`FUCKING ASSHOLES NEED TO SALT THEIR FUCKING FRIES MORE AAAAGGGGHHHHH`);
+    }
+    if(msg.content.includes("garfield eats")){
+        msg.channel.sendMessage(`FUCKING ASSHOLES NEED TO SALT THEIR FUCKING FRIES MORE AAAAGGGGHHHHH`);
+    }
+    if(msg.content.includes("society")){
+        msg.channel.sendMessage(`you get what you FUCKING DESERVE, ASSHOLE`);
+    }
+    if(msg.content.includes("SOCIETY")){
+        msg.channel.sendMessage(`you get what you FUCKING DESERVE, ASSHOLE`);
+    }
+    if(msg.content.includes("piss")){
+        msg.channel.sendMessage(`ONLY ON __SOME__ OCCASIONS`);
+    }
+    if(msg.content.includes("PISS")){
+        msg.channel.sendMessage(`ONLY ON __SOME__ OCCASIONS`);
+    }
+    if(msg.content.includes("dark skinned girls")){
+        msg.channel.sendMessage(`AAAGGGHHH MY FUCKING COCK`);
+    }
+    if(msg.content.includes("Dark skinned girls")){
+        msg.channel.sendMessage(`AAAGGGHHH MY FUCKING COCK`);
+    }
+    if(msg.content.includes("dark skin girls")){
+        msg.channel.sendMessage(`AAAGGGHHH MY FUCKING COCK`);
+    }
+    if(msg.content.includes("Dark skin girls")){
+        msg.channel.sendMessage(`AAAGGGHHH MY FUCKING COCK`);
+    }
+    if(msg.content.includes("DARK SKINNED GIRLS")){
+        msg.channel.sendMessage(`AAAGGGHHH MY FUCKING COCK`);
+    }
+    if(msg.content.includes("DARK SKIN GIRLS")){
+        msg.channel.sendMessage(`AAAGGGHHH MY FUCKING COCK`);
+    }
+    if(msg.content.includes("emilyxsmyth")){
+        msg.channel.sendMessage(`IF YOUR GF HAS ANXIETY SHE'S A FUCKING *__THOT__*`);
+    }
+    if(msg.content.includes("voyeurism")){
+        msg.channel.sendMessage(`AAAAGGGHHH MY YABBA DABBA DICK`);
+    }
+    if(msg.content.includes("Voyeurism")){
+        msg.channel.sendMessage(`AAAAGGGHHH MY YABBA DABBA DICK`);
+    }
+    if(msg.content.includes("YABBA DABBA DOO")){
+        msg.channel.sendMessage(`FUCK YEA BROTHA`);
+    }
+    if(msg.content.includes("Yabba Dabba Doo")){
+        msg.channel.sendMessage(`FUCK YEA BROTHA`);
+    }
+    if(msg.content.includes("yabba dabba doo")){
+        msg.channel.sendMessage(`FUCK YEA BROTHA`);
+    }
+    if(msg.content.includes("Ward's Odyssey")){
+        msg.channel.sendMessage(`FUCKING ASSHOLES SO WHAT IF ETIKA DIED HE WAS A NIGGER ANYWAYS MY YOUTUBE MAGNUM OPUS IS WAY MORE IMPORTANT`);
+    }
+    if(msg.content.includes("Wards Odyssey")){
+        msg.channel.sendMessage(`FUCKING ASSHOLES SO WHAT IF ETIKA DIED HE WAS A NIGGER ANYWAYS MY YOUTUBE MAGNUM OPUS IS WAY MORE IMPORTANT`);
+    }
+    if(msg.content.includes("ward's odyssey")){
+        msg.channel.sendMessage(`FUCKING ASSHOLES SO WHAT IF ETIKA DIED HE WAS A NIGGER ANYWAYS MY YOUTUBE MAGNUM OPUS IS WAY MORE IMPORTANT`);
+    }
+    if(msg.content.includes("wards odyssey")){
+        msg.channel.sendMessage(`FUCKING ASSHOLES SO WHAT IF ETIKA DIED HE WAS A NIGGER ANYWAYS MY YOUTUBE MAGNUM OPUS IS WAY MORE IMPORTANT`);
+    }
+    if(msg.content.includes("WARD'S ODYSSEY")){
+        msg.channel.sendMessage(`FUCKING ASSHOLES SO WHAT IF ETIKA DIED HE WAS A NIGGER ANYWAYS MY YOUTUBE MAGNUM OPUS IS WAY MORE IMPORTANT`);
+    }
+    if(msg.content.includes("WARDS ODYSSEY")){
+        msg.channel.sendMessage(`FUCKING ASSHOLES SO WHAT IF ETIKA DIED HE WAS A NIGGER ANYWAYS MY YOUTUBE MAGNUM OPUS IS WAY MORE IMPORTANT`);
+    }
+    if(msg.content.includes("reddit moment")){
+        msg.channel.sendMessage(`LAUGH`);
+    }
+    if(msg.content.includes("Reddit moment")){
+        msg.channel.sendMessage(`LAUGH`);
+    }
+    if(msg.content.includes("REDDIT MOMENT")){
+        msg.channel.sendMessage(`LAUGH`);
+    }
+    if(msg.content.includes("7granddad")){
+        msg.channel.sendMessage(`BEN GIVE ME BACK MY FUCKING @ YOU ASSHOLE`);
+    }
+    if(msg.content.includes("wet socks")){
+        msg.channel.sendMessage(`;))`);
+    }
+    if(msg.content.includes("Wet socks")){
+        msg.channel.sendMessage(`;))`);
+    }
+    if(msg.content.includes("WET SOCKS")){
+        msg.channel.sendMessage(`;))`);
+    }
+    if(msg.content.includes("antiukrane")){
+        msg.channel.sendMessage(`I'LL COME TO HIS SCHOOL AND SHOOT IT UP AND SHOOT HIM AND SHOOT MYSELF`);
+    }
+    if(msg.content.includes("Antiukrane")){
+        msg.channel.sendMessage(`I'LL COME TO HIS SCHOOL AND SHOOT IT UP AND SHOOT HIM AND SHOOT MYSELF`);
+    }
+})
+
+bot.login(token);
